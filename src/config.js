456368670888b8a9262bbdf963333bcd4bb6e55f
carlosxd753 <carlosxd753@gmail.com>
2022-09-29 15:06:34 -0500

    First commit

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const PORT = process.env.PORT || 3000;
+
+export const DB_HOST = process.env.DB_HOST || "localhost";
+export const DB_USER = process.env.DB_USER || "root";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "a95041b3";
+export const DB_NAME = process.env.DB_NAME || "usersdb";
+export const DB_PORT = process.env.DB_PORT || 3306;
